0f48473564fa6f89a6d0b4c1254de7aa57c28908
wk <819548734@qq.com>
2026-03-21 15:23:49 +0800

    修改

--- assets/script/DishBigImg.ts
+++ assets/script/DishBigImg.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, isValid, resources, Sprite, SpriteFrame } from "cc";
+import { _decorator, clamp, Component, EventTouch, isValid, Node, resources, Sprite, SpriteFrame, v2, v3, Vec2, Vec3 } from "cc";
 import { UIOp2DUtils } from "./UIOp2DUtils";
 const { ccclass, property } = _decorator;
 
@@ -7,12 +7,21 @@ export class DishBigImg extends Component {
     @property(Sprite)
     private grpIcon: Sprite = null;
 
+    private originalTouchDistance: number = -1;
+    private originalNodeScale: Vec3;
+
     start() {
         this.onAddEventListener();
     }
 
     private onAddEventListener() {
         UIOp2DUtils.createScaleBtn(this.node, this.onClickClose, this, { bStatus: false });
+        this.grpIcon.node.off(Node.EventType.TOUCH_MOVE, this.onMapTouchMoveEvent, this);
+        this.grpIcon.node.off(Node.EventType.TOUCH_END, this.onMapTouchEndEvent, this);
+        this.grpIcon.node.off(Node.EventType.TOUCH_CANCEL, this.onMapTouchEndEvent, this);
+        this.grpIcon.node.on(Node.EventType.TOUCH_MOVE, this.onMapTouchMoveEvent, this);
+        this.grpIcon.node.on(Node.EventType.TOUCH_END, this.onMapTouchEndEvent, this);
+        this.grpIcon.node.on(Node.EventType.TOUCH_CANCEL, this.onMapTouchEndEvent, this);
     }
 
     public updateView(strUrl: string) {
@@ -25,5 +34,29 @@ export class DishBigImg extends Component {
 
     private onClickClose() {
         this.node.destroy();
+    }
+
+    private onMapTouchMoveEvent(event: EventTouch) {
+        let touchs = event.getTouches();
+        if (touchs.length < 2) {
+            return;
+        }
+        let temp = v2();
+        Vec2.subtract(temp, touchs[0].getLocation(), touchs[1].getLocation());
+        let distance = temp.length();
+        if (this.originalTouchDistance == -1) {
+            this.originalTouchDistance = distance;
+            this.originalNodeScale = this.grpIcon.node.scale.clone();
+        }
+        let targetScale = v3();
+        let scale = distance / this.originalTouchDistance;
+        Vec3.multiplyScalar(targetScale, this.originalNodeScale, scale);
+        scale = targetScale.x;
+        scale = clamp(scale, 0.5, 1);
+        UIOp2DUtils.setScale(this.grpIcon.node, scale, scale);
+    }
+
+    private onMapTouchEndEvent() {
+        this.originalTouchDistance = -1;
     }
 }
--- assets/script/MenuPrefab.ts
+++ assets/script/MenuPrefab.ts
@@ -1,11 +1,10 @@
 import { _decorator, Component, instantiate, Label, Node, Prefab, ScrollView } from "cc";
 import { DishBigImg } from "./DishBigImg";
-import { DishExplainDialog } from "./DishExplainDialog";
+import { DishItem } from "./DishItem";
+import { DishTypeItem } from "./DishTypeItem";
 import { eventManager } from "./EventManger";
 import { DishData, MenuManage } from "./MenuManage";
 import { UIOp2DUtils } from "./UIOp2DUtils";
-import { DishTypeItem } from "./DishTypeItem";
-import { DishItem } from "./DishItem";
 const { ccclass, property } = _decorator;
 
 @ccclass("MenuPrefab")
@@ -14,12 +13,8 @@ export class MenuPrefab extends Component {
     private glbDishNum: Label = null;
     @property(Node)
     private btnSure: Node = null;
-    @property(Node)
-    private btnAddDish: Node = null;
     @property(Prefab)
     private dishSelectPrefab: Prefab = null;
-    @property(Prefab)
-    private dishExplainDialog: Prefab = null;
     @property(Prefab)
     private dishBigImg: Prefab = null;
     @property(ScrollView)
@@ -39,20 +34,19 @@ export class MenuPrefab extends Component {
 
     private initView() {
         let vDishType: string[] = MenuManage.ins.vDishType;
-        for(let i=0;i<vDishType.length; i++){
+        for (let i = 0; i < vDishType.length; i++) {
             let item = this.scrollType.content.children[i];
-            if(!item){
+            if (!item) {
                 item = instantiate(this.dishTypeItem);
                 this.scrollType.content.addChild(item);
             }
             item.active = true;
-            item.getComponent(DishTypeItem).updateView(i,vDishType[i])
+            item.getComponent(DishTypeItem).updateView(i, vDishType[i]);
         }
     }
 
     private onAddEventListener() {
         UIOp2DUtils.createScaleBtn(this.btnSure, this.onClickBtnSure, this);
-        UIOp2DUtils.createScaleBtn(this.btnAddDish, this.onClickBtnAddDish, this);
         eventManager.addEvent("dish_change", this.dishChangeEvent.bind(this), this);
         eventManager.addEvent("add_dish", this.addDishEvent.bind(this), this);
         eventManager.addEvent("show_icon", this.showIconEvent.bind(this), this);
@@ -64,18 +58,18 @@ export class MenuPrefab extends Component {
         this.glbDishNum.string = `当前已点菜品数：${vSelectDish.length}`;
         let nSelectedId: number = MenuManage.ins.nSelectIndex;
         let vDishDataOfId: DishData[] = MenuManage.ins.getMapDishDataBySelect(nSelectedId);
-        for(let i=0;i<vDishDataOfId.length; i++){
+        for (let i = 0; i < vDishDataOfId.length; i++) {
             let item = this.scrollDish.content.children[i];
-            if(!item){
+            if (!item) {
                 item = instantiate(this.dishItem);
                 this.scrollDish.content.addChild(item);
             }
             item.active = true;
             item.getComponent(DishItem).updateView(vDishDataOfId[i]);
         }
-        for(let j=vDishDataOfId.length; j<this.scrollDish.content.children.length; j++){
+        for (let j = vDishDataOfId.length; j < this.scrollDish.content.children.length; j++) {
             this.scrollDish.content.children[j].active = false;
-        } 
+        }
     }
 
     private onClickBtnSure() {
@@ -83,32 +77,11 @@ export class MenuPrefab extends Component {
         this.node.addChild(item);
     }
 
-    private onClickBtnAddDish() {
-        let item = instantiate(this.dishExplainDialog);
-        this.node.addChild(item);
-        item.getComponent(DishExplainDialog).updateData("选择想要新增的菜品，后续添加", (strDesc: string) => {
-            MenuManage.ins.handleCopy(strDesc);
-        });
-    }
-
     private dishChangeEvent() {
         this.updateView();
     }
 
-    private addDishEvent(arg: any[]) {
-        let dishData: DishData = arg[0];
-        let item = instantiate(this.dishExplainDialog);
-        this.node.addChild(item);
-        item.getComponent(DishExplainDialog).updateData("添加菜品备注，也可不备注", (strDesc: string) => {
-            let newDishData: DishData = {
-                id: dishData.id,
-                name: dishData.name,
-                strUrl: dishData.strUrl,
-                strExplain: strDesc,
-            };
-            MenuManage.ins.addSelectDish(newDishData);
-        });
-    }
+    private addDishEvent(arg: any[]) {}
 
     private showIconEvent(arg: any[]) {
         let strUrl: string = arg[0];
@@ -119,7 +92,7 @@ export class MenuPrefab extends Component {
 
     private typeSelectEvent() {
         this.updateView();
-        for(let i=0;i<this.scrollType.content.children.length; i++){
+        for (let i = 0; i < this.scrollType.content.children.length; i++) {
             this.scrollType.content.children[i].getComponent(DishTypeItem).changeSelect();
         }
     }
